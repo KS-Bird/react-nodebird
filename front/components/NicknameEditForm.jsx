de0f1434919567bc49895e959399e3c79a4692fc
Kyuseok Park <dosim0106@gmail.com>
2023-06-23 04:11:38 +0900

    Style

--- front/components/NicknameEditForm.jsx
+++ front/components/NicknameEditForm.jsx
@@ -1,8 +1,7 @@
 import { Form, Input } from "antd";
 import { useMemo } from 'react';
 
-
-const NicknameEditForm = ({ header, data }) => {
+const NicknameEditForm = () => {
   const style = useMemo(() => ({ marginBottom: '20px', border: '1px solid #d9d9d9', padding: '20px' }), []);
 
   return (
