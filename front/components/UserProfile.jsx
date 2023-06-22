3e5b2c7297d550809b1d41cb3a502ed538ad9651
Kyuseok Park <dosim0106@gmail.com>
2023-06-23 00:54:02 +0900

    Feat: UserProfile

--- front/components/UserProfile.jsx
+++ front/components/UserProfile.jsx
@@ -1,6 +1,29 @@
-const UserProfile = () => {
+import { Card, Avatar, Button } from 'antd';
+import { useCallback } from 'react';
+import styled from 'styled-components';
+
+const LogOutButton = styled(Button)`
+  margin-top: 15px;
+`;
+
+const UserProfile = ({ setIsLoggedIn }) => {
+  const onLogOut = useCallback(() => {
+    setIsLoggedIn(false);
+  }, [])
+
   return (
-    <></>
+    <Card
+      actions={[
+        <div key="twit">짹짹<br /></div>,
+        <div key="following" >팔로잉<br /></div>,
+        <div key="follower" >팔로워<br /></div>,
+      ]}>
+      <Card.Meta
+        avatar={<Avatar>KS</Avatar>}
+        title="Kyuseok Park"
+      />
+      <LogOutButton onClick={onLogOut}>로그아웃</LogOutButton>
+    </Card >
   );
 }
 
