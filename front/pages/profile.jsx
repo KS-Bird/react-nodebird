4e4ce933989d66c5f15ee257475b2012f8013192
Kyuseok Park <dosim0106@gmail.com>
2023-06-23 02:23:59 +0900

    Feat: profile page

--- front/pages/profile.jsx
+++ front/pages/profile.jsx
@@ -1,13 +1,23 @@
 import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
 
 const Profile = () => {
+
+  const followingList = [{ nickname: '팔로잉' }, { nickname: '바보' }, { nickname: '노버' }];
+  const followerList = [{ nickname: '팔로워' }, { nickname: '바보' }, { nickname: '노버' }];
+
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
-      <AppLayout>내 프로필</AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 }
