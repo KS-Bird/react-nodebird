1173229332eb5b6f25d3f4ad552cf5a881be4682
Kyuseok Park <dosim0106@gmail.com>
2023-06-23 00:55:57 +0900

    Refactor: inline style to styled component

--- front/components/AppLayout.jsx
+++ front/components/AppLayout.jsx
@@ -2,12 +2,17 @@ import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Menu, Input, Row, Col } from 'antd';
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
 
 import UserProfile from '../components/UserProfile';
 import LoginForm from '../components/LoginForm';
 
 const AppLayout = ({ children }) => {
-  const [isLoggedIn, setIsloggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div>
@@ -19,7 +24,7 @@ const AppLayout = ({ children }) => {
           <Link href="/profile"><a>프로필</a></Link>
         </Menu.Item>
         <Menu.Item>
-          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+          <SearchInput enterButton />
         </Menu.Item>
         <Menu.Item>
           <Link href="/signup"><a>회원가입</a></Link>
@@ -28,7 +33,7 @@ const AppLayout = ({ children }) => {
       {/* gutter: column간의 간격 xs: 모바일, md: 작은 데탑 */}
       <Row gutter={8}>
         <Col xs={24} md={6} > {/* 한줄의 24등분중에 모바일은 100% 차지 / 데탑은 25% 차지한다는 뜻*/}
-          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+          {isLoggedIn ? <UserProfile setIsLoggedIn={setIsLoggedIn} /> : <LoginForm setIsLoggedIn={setIsLoggedIn} />}
         </Col>
         <Col xs={24} md={12} >
           {children}
