4c2629a06970b3ed0eb5c13c5a7ea2cd323166fb
Kyuseok Park <dosim0106@gmail.com>
2023-06-29 04:52:33 +0900

    Feat: PostForm window

---
+++ front/components/PostForm.jsx
@@ -0,0 +1,47 @@
+import { Form, Input, Button } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { useCallback, useRef, useState } from 'react';
+import { addPost } from '../reducers/post';
+
+const PostForm = () => {
+  const { imagePaths } = useSelector((state) => state.post);
+  const dispatch = useDispatch();
+  const [text, setText] = useState('');
+  const onChangeText = useCallback((e) => {
+    setText(e.target.value);
+  });
+  const imageInput = useRef();
+  const onClickImageUpload = useCallback(() => {
+    imageInput.current.click();
+  }, [imageInput.current]);
+  const onSubmit = useCallback(() => {
+    dispatch(addPost);
+    setText('');
+  }, []);
+
+  return (
+    <Form style={{ margin: '10px 0 20px' }} encType="multipart/form-data" onFinish={onSubmit}>
+      <Input.TextArea
+        value={text}
+        onChange={onChangeText}
+        maxLength={140}
+        placeholder="어떤 신기한 일이 있었나요?"
+      />
+      <div>
+        <input type="file" multiple ref={imageInput} style={{ display: "none" }} />
+        <Button onClick={onClickImageUpload}>이미지 업로드</Button>
+        <Button type="primary" style={{ float: 'right' }} htmlType="submit">짹짹</Button>
+      </div>
+      <div>
+        {imagePaths.map((v) => ( // 업로드할 이미지들
+          <div key={v} style={{ display: 'inline-block' }}>
+            <img src={v} alt={v} style={{ width: '200px' }} />
+            <Button>제거</Button>
+          </div>
+        ))}
+      </div>
+    </Form>
+  );
+}
+
+export default PostForm;
