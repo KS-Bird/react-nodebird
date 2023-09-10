7af17e159fa625b806108785740ffe37a9ddab37
Kyuseok Park <dosim0106@gmail.com>
2023-09-11 00:47:00 +0900

    프로덕트 빌드 설정

---
+++ front/next.config.js
@@ -0,0 +1,16 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  compress: true,
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === 'production';
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      // hidden-source-map : 소스코드 숨겨짐
+      devtool: prod ? 'hidden-source-map' : 'eval',
+    };
+  },
+});
