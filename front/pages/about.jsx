4b2192ba3ed7b39e5618a38ee38d61e25d7a0a93
Kyuseok Park <dosim0106@gmail.com>
2023-09-15 03:33:11 +0900

    about페이지 getServerSideProps로 수정

--- front/pages/about.jsx
+++ front/pages/about.jsx
@@ -2,10 +2,11 @@ import { useSelector } from "react-redux";
 import Head from 'next/head';
 import { END } from 'redux-saga';
 import { Card, Avatar } from 'antd';
+import axios from "axios";
 
 import wrapper from "../store/configureStore";
 import AppLayout from "../components/AppLayout";
-import { LOAD_USER_REQUEST } from "../reducers/user";
+import { LOAD_USER_REQUEST, LOAD_MY_INFO_REQUEST } from "../reducers/user";
 
 const About = () => {
   const userInfo = useSelector((store) => store.user.userInfo);
@@ -34,13 +35,18 @@ const About = () => {
   );
 };
 
-export const getStaticProps = wrapper.getStaticProps((store) => async () => {
+export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ req }) => {
+  axios.defaults.headers.Cookie = req?.headers.cookie;
+  store.dispatch({
+    type: LOAD_MY_INFO_REQUEST,
+  });
   store.dispatch({
     type: LOAD_USER_REQUEST,
     data: 3,
   });
   store.dispatch(END);
   await store.sagaTask.toPromise();
+  axios.defaults.headers.Cookie = '';
 });
 
 export default About;
